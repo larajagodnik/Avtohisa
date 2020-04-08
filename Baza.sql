c7a2a49933df63ec3a4ef96d50a0dbcd735294ee
larajagodnik <39483252+larajagodnik@users.noreply.github.com>
2020-04-09 00:17:57 +0200

    Update Baza.sql
    manjši popravki imen stolpcev

--- Baza.sql
+++ Baza.sql
@@ -1,6 +1,6 @@
 CREATE TABLE zaposleni (
   emso TEXT PRIMARY KEY,
-  telefon TEXT,
+  telefon INTEGER,
   naslov TEXT,
   placa INTEGER
 );
@@ -11,35 +11,35 @@ CREATE TABLE prodajalec(
   emso TEXT PRIMARY KEY REFERENCES zaposleni(emso)
 );
 CREATE TABLE avto(
-  id TEXT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   barva TEXT,
   tip TEXT,
   znamka TEXT,
   cena INTEGER
 );
 CREATE TABLE priprava(
-  id TEXT PRIMARY KEY,
-  serviser TEXT NOT NULL REFERENCES serviser(emso)
+  id INTEGER PRIMARY KEY,
+  id_avto INTEGER NOT NULL REFERENCES avto(id)
+  id_serviser TEXT NOT NULL REFERENCES serviser(emso)
 );
 CREATE TABLE servis(
-  id TEXT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   datum DATE,
-  tip TEXT,
-  serviser TEXT NOT NULL REFERENCES serviser(emso)
+  tip_servisa TEXT,
+  id_serviser INTEGER NOT NULL REFERENCES serviser(emso)
+  id_avto INTEGER NOT NULL REFERENCES avto(id)
 );
-CREATE TABLE rabljen(
-  id TEXT PRIMARY KEY REFERENCES avto(id),
+CREATE TABLE rabljeni(
+  id INTEGER PRIMARY KEY REFERENCES avto(id),
   st_kilometrov INTEGER,
-  leto_izdelave DATE,
-  priprava TEXT REFERENCES priprava(id)
+  leto_izdelave DATE
 );
 CREATE TABLE novi(
-  id TEXT PRIMARY KEY REFERENCES avto(id),
-  priprava TEXT REFERENCES priprava(id)
+  id INTEGER PRIMARY KEY REFERENCES avto(id),
 );
 CREATE TABLE prodaja(
-  id TEXT PRIMARY KEY,
-  avto TEXT REFERENCES avto(id),
+  id INTEGER PRIMARY KEY,
+  id_avto TEXT REFERENCES avto(id),
   cena INTEGER,
   datum DATE,
   nacim_placila TEXT,

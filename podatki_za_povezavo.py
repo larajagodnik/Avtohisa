3852c01d2cb02382bac34c2186e5a122e4ff8407
Jan Šifrer <jan.sifrer1@gmail.com>
2020-04-18 17:49:43 +0200

    dodal navodila za priklop do baze.

---
+++ podatki_za_povezavo.py
@@ -0,0 +1,7 @@
+#Ustvari nov dokument conf_baza.py in v njega zapiši:
+host = "baza.fmf.uni-lj.si"
+dbname = "sem2020_jansi"
+user = "tvoje uporabniško ime"
+password = "tvoje geslo"
+
+#Preden commitaš se prepriči da datoteke conf_baza.py res ni možno commitat!!

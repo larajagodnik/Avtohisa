ed26fd73256a4aa51923ae2e12d61012791b4838
larajagodnik <lara.jagodnik@gmail.com>
2020-04-19 09:39:58 +0200

    test

--- Baza/test.py
+++ Baza/test.py
@@ -9,7 +9,6 @@ with psycopg2.connect(conn_string) as con:
     #cur.execute("INSERT INTO avto VALUES(101, 'rumena', 'karavan', 'peguot', 200, TRUE)")
     #cur.execute("INSERT INTO avto VALUES(102, 'rumena', 'karavan', 'peguot', 200, True)")
     #cur.execute("INSERT INTO avto VALUES(103, 'rumena', 'karavan', 'peguot', 200, true)")
-    cur.execute("INSERT INTO avto VALUES(104, 'rumena', 'karavan', 'peguot', 200, true)")
-
+    #cur.execute("INSERT INTO avto VALUES(104, 'rumena', 'karavan', 'peguot', 200, true)")
 
     #con.commit()

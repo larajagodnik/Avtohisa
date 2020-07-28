6a5f411decf1644400fcdd74d91847b8f1ef6778
larajagodnik <lara.jagodnik@gmail.com>
2020-07-28 17:51:31 +0200

    filtriranje po tabeli dela

--- static/index.js
+++ static/index.js
@@ -6,15 +6,35 @@ function filtertabela(el){
 
     var vrednost = el.options[el.selectedIndex].value;
     let tabela = document.getElementById('tabela_avtov');
-    [...tabela.rows].forEach(vrstica => {         
-
-      if(seUjema(vrstica, stolpec, vrednost) || vrednost == "null") {
-          vrstica.style.display = 'inherit'
-          
-       } else {
-         vrstica.style.display = 'none'
-       }
+    [...tabela.rows].forEach(vrstica => {  
+        vrstica.style.display = 'inherit'
     })
+    var leto_el = document.getElementById('leto_izdelave');
+    var barva_el = document.getElementById('barve');
+    var tip_el = document.getElementById('tipi');
+    var znamka_el = document.getElementById('znamka');
+    var izbrane_vrednosti = [ leto_el.options[leto_el.selectedIndex].value, znamka_el.options[znamka_el.selectedIndex].value,  tip_el.options[tip_el.selectedIndex].value, barva_el.options[barva_el.selectedIndex].value];
+    var count = 4;
+   
+    izbrane_vrednosti.forEach(vrednost => {
+      [...tabela.rows].forEach(vrstica => {         
+        let vrednost_v_tabeli = vrstica.cells[count].innerText
+        
+        if(vrednost != 'null'){
+        
+          if(vrednost_v_tabeli.toLocaleLowerCase().indexOf(vrednost.toLocaleLowerCase()) >= 0) {
+          
+            
+              
+          }else{
+          
+            vrstica.style.display = 'none'
+          }
+        }
+      })
+      count--
+    })
+    
 }
 
   function seUjema(vrstica, stolpec, vrednost) {
